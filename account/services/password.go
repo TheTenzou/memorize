b38bc5248b599310173e636ef56c305d22ff3b2a
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-10 19:23:37 +0300

    Add password encryption

---
+++ account/services/password.go
@@ -0,0 +1,50 @@
+package services
+
+import (
+	"crypto/rand"
+	"encoding/hex"
+	"fmt"
+	"strings"
+
+	"golang.org/x/crypto/scrypt"
+)
+
+func hashPassword(password string) (string, error) {
+	salt := make([]byte, 32)
+
+	_, err := rand.Read(salt)
+	if err != nil {
+		return "", err
+	}
+
+	hash, err := scrypt.Key([]byte(password), salt, 32768, 8, 1, 32)
+	if err != nil {
+		return "", err
+	}
+
+	hashedPassword := fmt.Sprintf(
+		"%s.%s",
+		hex.EncodeToString(hash),
+		hex.EncodeToString(salt),
+	)
+
+	return hashedPassword, nil
+}
+
+func comparePassword(storedPassword, suppliedPassword string) (bool, error) {
+	passwordAndSalt := strings.Split(storedPassword, ".")
+
+	salt, err := hex.DecodeString(passwordAndSalt[1])
+
+	if err != nil {
+		return false, fmt.Errorf("unable to verify user password")
+	}
+
+	hash, err := scrypt.Key([]byte(suppliedPassword), salt, 32768, 8, 1, 32)
+
+	if err != nil {
+		return false, fmt.Errorf("unable to verify user password")
+	}
+
+	return hex.EncodeToString(hash) == passwordAndSalt[0], nil
+}
