9ba39a5339201217bb8b373602b78dc120bcebf9
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-10 19:47:15 +0300

    Add user repository

---
+++ account/repository/pg_user_repository.go
@@ -0,0 +1,55 @@
+package repository
+
+import (
+	"context"
+	"log"
+	"memorize/models"
+	"memorize/models/apperrors"
+
+	"github.com/google/uuid"
+	"github.com/jmoiron/sqlx"
+	"github.com/lib/pq"
+)
+
+type PGUserRepository struct {
+	DB *sqlx.DB
+}
+
+func NewUserRepository(db *sqlx.DB) models.UserRepository {
+	return &PGUserRepository{
+		DB: db,
+	}
+}
+
+func (this *PGUserRepository) Create(ctx context.Context, user *models.User) error {
+	query := "INSERT INTO users (email, password) VALUES ($1, $2) RETURNING *"
+
+	if err := this.DB.Get(user, query, user.Login, user.Password); err != nil {
+
+		if err, ok := err.(*pq.Error); ok && err.Code.Name() == "unique_violation" {
+			log.Printf(
+				"Could not create a user with login: %v. Reason %v\n",
+				user.Login,
+				err.Code.Name(),
+			)
+
+			return apperrors.NewConflict("login", user.Login)
+		}
+
+		log.Printf("Could not create a user with login: %v. Reason: %v", user.Login, err)
+	}
+
+	return nil
+}
+
+func (this *PGUserRepository) FindByID(ctx context.Context, uid uuid.UUID) (*models.User, error) {
+	user := &models.User{}
+
+	query := "SELECT * FROM users WHERE uid=&1"
+
+	if err := this.DB.Get(user, query, uid); err != nil {
+		return user, apperrors.NewNotFound("uid", uid.String())
+	}
+
+	return user, nil
+}
