865e4be9e5e76040d49590c4108f4a9a55246652
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-12 22:27:52 +0300

    Add comments to interfaces

--- account/models/interfaces.go
+++ account/models/interfaces.go
@@ -9,23 +9,30 @@ import (
 
 // UserService defines methods the handler layer expects
 type UserService interface {
+	// fetch user by uid
 	GetUser(context.Context, uuid.UUID) (*User, error)
+	// signup user if login avaliable
 	Signup(context.Context, *User) error
 }
 
 // TokenService defines methods the handler layer expects
 type TokenService interface {
+	// create new pair of tokens
 	NewPairFromUser(context.Context, *User, string) (*TokenPair, error)
 }
 
 // UserRepository defines methods the service layer expects
 type UserRepository interface {
+	// fetch user by id from database
 	FindByID(context.Context, uuid.UUID) (*User, error)
+	// create user record in database
 	Create(context.Context, *User) error
 }
 
 // TokenRepository defines methods it expects a repository
 type TokenRepository interface {
+	// stores a refresh token with an expiry time
 	SetRefreshToken(ctx context.Context, userID string, tokenID string, expiresIn time.Duration) error
+	// delete old refresh tokens
 	DeleteRefreshToken(ctx context.Context, userID string, previousTokenID string) error
 }
