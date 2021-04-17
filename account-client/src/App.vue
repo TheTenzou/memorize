940310498462d6d81e3a6914b632b528db06e30e
TheTenzou <61962705+TheTenzou@users.noreply.github.com>
2021-04-17 17:24:20 +0300

    Add tailwind

--- account-client/src/App.vue
+++ account-client/src/App.vue
@@ -1,7 +1,9 @@
 <template>
-  <h1>Scaffolded App Works Well!</h1>
-  <h3 v-if="errorCode">Error code: {{ errorCode }}</h3>
-  <h3 v-if="errorMessage">{{ errorMessage }}</h3>
+  <h1 class="text-4xl font-bol text-center my-12">Scaffolded App Works Well!</h1>
+  <h3 class="text-sl text-center" v-if="errorCode">
+    Error code: {{ errorCode }}
+  </h3>
+  <h3 class="text-center" v-if="errorMessage">{{ errorMessage }}</h3>
 </template>
 
 <script>
@@ -30,14 +32,3 @@ export default defineComponent({
   },
 })
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
